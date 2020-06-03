4b7b2a489845ea831cf121db909531614d37738c
IanKoMagic <ian.korniev@gmail.com>
2020-06-03 16:32:33 +0300

    rr

--- index.js
+++ index.js
@@ -1,59 +1,24 @@
 "use strict";
 
-const name = "Ian";
-let age = 14;
+let crlsRoot = document.querySelector(".ant-carousel");
+console.log(crlsRoot);
 
-console.log(age);
-age = 12;
-console.log(age);
+let crlsList = document.querySelector(".ant-carousel-list");
+console.log(crlsList);
 
-let message; //without value
-message = "Hello!";
-console.log(message);
-console.log(typeof message); //string
-console.log(typeof age); //number
+let crlsElements = document.querySelectorAll(".ant-carousel-element");
+console.log(crlsElements);
 
-console.log(typeof true); //boolean
-console.log(typeof false); //boolean
+let crlsElemFirst = document.querySelector(".ant-carousel-element");
+console.log(crlsElemFirst);
 
-console.log(typeof null); //object special problem of js//
+let leftArrow = document.querySelector("div.ant-carousel-arrow-left");
+console.log(leftArrow);
 
-const object = {
-  name: "Ian",
-  age: 14,
-};
-console.log(object);
+let rightArrow = document.querySelector("div.ant-carousel-arrow-right");
+console.log(rightArrow);
 
-const funcExp = function (a, b, c) {
-  return console.log(a + b + c);
-};
-funcExp(3, 2, 1);
+let indicatorDots = document.querySelector(".ant-carousel-dots");
+console.log(indicatorDots);
 
-const arrowFunc = (a, b, c) => console.log(a + b + c);
-arrowFunc(3, 6, 10);
-
-const breadPrice = 40;
-const milkPrice = 30;
-const eggsPrice = 28;
-const orangesPrice = 56;
-const bananasPrice = 38;
-const applesPrice = 98;
-
-const totalPrice = function (prod1, prod2, prod3, prod4, prod5) {
-  return console.log(prod1 + prod2 + prod3 + prod4 + prod5);
-};
-totalPrice(breadPrice + milkPrice + eggsPrice + orangesPrice + bananasPrice);
-
-const sum = function (count = 1, price = 1) {
-  return console.log(count * price);
-};
-sum(2, 4);
-sum(2, breadPrice);
-
-// alert();
-
-// const answer = confirm("Do u want to learn JS?");
-// console.log(answer);
-
-// const data = prompt("Write your age");
-// console.log(data);
+//hhhhhhhhhhhhhhhhhhhhhhhhhhhh
